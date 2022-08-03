f2b10dc6cb478bf276873961b696e54d8ee24b17
Elias Torres <elias.torressn@outlook.com>
2022-08-03 18:20:28 -0300

    Se añadió página de error en caso de no tener edad suficiente

---
+++ archivos/logic.js
@@ -0,0 +1,9 @@
+
+
+let edad=confirm("Confirmo que soy mayor de edad para ingresar al sitio (+18).").toString();
+if(edad==="true"){
+    alert("Gracias por confirmar")
+}else{
+    
+    window.location.href = "";
+}
