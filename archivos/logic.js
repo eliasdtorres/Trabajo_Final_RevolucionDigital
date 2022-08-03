07fbbb75d1a1e09e98b5cf42eaf5aef4aa37140d
Elias Torres <elias.torressn@outlook.com>
2022-08-03 18:49:21 -0300

    enrrutamiento en logic

--- archivos/logic.js
+++ archivos/logic.js
@@ -1,10 +1,14 @@
 
 let edad = confirm("Confirmo que soy mayor de edad para ingresar al sitio (+18).").toString();
+
+
 if (edad === "true") {
+
     alert("Gracias por confirmar")
 
-} else {
+} else if(edad==="false"){
 
     window.location.href = "https://eliasdtorres.github.io/Trabajo_Final_RevolucionDigital/error.html";
 
 }
+console.log(edad);
