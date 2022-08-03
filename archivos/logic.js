2aa8ec552ac001eaabaa6ac63446df2712b6f4ee
Elias Torres <elias.torressn@outlook.com>
2022-08-03 18:40:50 -0300

    err

--- archivos/logic.js
+++ archivos/logic.js
@@ -3,4 +3,8 @@ let edad = confirm("Confirmo que soy mayor de edad para ingresar al sitio (+18).
 if (edad === "true") {
     alert("Gracias por confirmar")
 
+} else {
+
+    window.location.href = "https://eliasdtorres.github.io/Trabajo_Final_RevolucionDigital/error.html";
+
 }
