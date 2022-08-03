c9249837a1ccf46d5f6f9efad249bf80248d89b1
Elias Torres <elias.torressn@outlook.com>
2022-08-03 18:37:03 -0300

    Correción de página de error

--- archivos/logic.js
+++ archivos/logic.js
@@ -1,9 +1,10 @@
 
-
-let edad=confirm("Confirmo que soy mayor de edad para ingresar al sitio (+18).").toString();
-if(edad==="true"){
+let edad = confirm("Confirmo que soy mayor de edad para ingresar al sitio (+18).").toString();
+if (edad === "true") {
     alert("Gracias por confirmar")
-}else{
-    
+
+} else {
+
     window.location.href = "";
+
 }
